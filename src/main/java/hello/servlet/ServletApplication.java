67ec7560226be2bb5992fe58feb939af4d41c477
sunJ <sspure1214@gmail.com>
2025-03-08 13:12:28 +0900

    servlet 체험 http 헤더값 가져오기

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,11 @@
 package hello.servlet;
 
+import jakarta.servlet.annotation.ServletSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
