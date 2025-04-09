f303c6402a8b755a6dc90d6aea83fa984a77fb6c
sunJ <sspure1214@gmail.com>
2025-04-09 23:03:46 +0900

    Add JDBC gradle project files

---
+++ spring_db/jdbc/src/test/java/hello/myJdbc/MyJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.myJdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
