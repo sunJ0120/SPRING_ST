72d7a143d4eb3789a1407bde7f5e71da23d1dbc8
sunJ <sspure1214@gmail.com>
2025-04-13 14:30:07 +0900

    [ch 02] jdbc 방식으로 repository update, delete 기능 구현

--- spring_db/jdbc/src/main/java/hello/myJdbc/repository/MemberRepositoryV0.java
+++ spring_db/jdbc/src/main/java/hello/myJdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.myJdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /*
 jdbc connection을 이용한 db 연동 및 레포지토리 작업
@@ -30,6 +31,75 @@ public class MemberRepositoryV0 {
             close(con,pstmt,null);
         }
     }
+
+    public Member findById(String memberId) throws SQLException{
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();//실행하는것, 쿼리 결과를 반환
+
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con,pstmt,rs);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException{
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize={}", resultSize); //몇개 업데이트 되었는지 나옴.
+        } catch (SQLException e){
+            log.error("db error", e);
+        } finally {
+            close(con,pstmt, null);
+        }
+    }
+
+    public void delete(String memberId) throws SQLException{
+        String sql = "delete from member where member_id=?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            pstmt.executeUpdate(); //delete 수행
+        } catch (SQLException e){
+            log.error("db error", e);
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
 
     private void close(Connection con, Statement stmt, ResultSet rs){
         if(rs != null){
--- spring_db/jdbc/src/test/java/hello/myJdbc/repository/MemberRepositoryV0Test.java
+++ spring_db/jdbc/src/test/java/hello/myJdbc/repository/MemberRepositoryV0Test.java
@@ -1,20 +1,39 @@
 package hello.myJdbc.repository;
 
 import hello.myJdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
     MemberRepositoryV0 repository = new MemberRepositoryV0();
-
     @Test
     void crud() throws SQLException{
         //save
-        Member member = new Member("memberV0", 10000);
+        Member member = new Member("memberV100", 10000);
         repository.save(member);
-    }
 
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        assertThat(findMember).isEqualTo(member); //id로 찾은 member가 저장한 member랑 같은 객체인지 비교
+
+        //update : money : 10000-> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        //new NoSuchElementException
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class); //NoSuchElementException.class 이거에 속하는지를 본다.
+    }
 }
