b91822304908121a728a73027e32b90ab93efa4d
sunJ0120 <sspure1214@gmail.com>
2025-08-03 17:26:25 +0900

    Study : [실전 JPA 1] 섹션 5. 완료 - Item도 마찬가지로 member와 비슷하게 진행한다. - stock 같은 경우는 Item에 명시되어 있으므로, 도메인 주도적으로 내부에 비즈니스 메서드를 작성하는 것이 객체 지향적이고 좋다. - stock이 0이하일때는 custom Exception을 발생시키도록 한다.

--- spring_jpa/jpa1/src/main/java/jpabook/jpa1/entity/Item.java
+++ spring_jpa/jpa1/src/main/java/jpabook/jpa1/entity/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpa1.entity;
 
 import jakarta.persistence.*;
+import jpabook.jpa1.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,19 @@ public class Item {
 
     @ManyToMany(mappedBy = "items") // 다대다 관계, 연관관계의 주인이 아니다. 여기서는 카테고리를 연관관계 주인으로 둠.
     List<Category> categories = new ArrayList<>(); // 카테고리
+
+    //==비즈니스 로직==//
+    // 재고 수량을 관리하는 비즈니스 로직을 추가한다.
+    // 엔티티 자체가 할 수 있는 일은, 엔티티 안에 넣는 것이 좋다. -> DDD
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity; // 재고 수량 증가
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity; // 재고 수량 감소
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock"); // 재고 부족 예외 처리
+        }
+        this.stockQuantity = restStock; // 재고 수량 업데이트
+    }
 }
---
+++ spring_jpa/jpa1/src/main/java/jpabook/jpa1/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpabook.jpa1.service;
+
+import jpabook.jpa1.entity.Item;
+import jpabook.jpa1.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item); // 아이템을 저장하는 메서드
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll(); // 모든 아이템을 조회하는 메서드
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId); // 특정 아이템을 ID로 조회하는 메서드
+    }
+}
