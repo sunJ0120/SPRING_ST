7437e8a4e86d9ce54aab2739dd2f61d0955c9678
sunJ0120 <sspure1214@gmail.com>
2025-08-11 21:18:57 +0900

    Feat : [실전! Query DSL] 섹션1. 프로젝트 환경 설정 완료 - QueryDSL을 사용하기 위해서는 querydsl-jpa, -apt (생성을 위함), 그외 annotationProcessor 관련 설정을 해야 한다.

--- spring_jpa/query_dsl/src/test/java/jpabook/query_dsl/QueryDslApplicationTests.java
+++ spring_jpa/query_dsl/src/test/java/jpabook/query_dsl/QueryDslApplicationTests.java
@@ -1,13 +1,37 @@
 package jpabook.query_dsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jpabook.query_dsl.entity.Hello;
+import jpabook.query_dsl.entity.QHello;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class QueryDslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
-	}
+		Hello hello = new Hello();
+		em.persist(hello);
 
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		// QHello는 Querydsl이 생성한 메타 모델 클래스입니다. 항상 쿼리 관련된건 이걸 생성해서 실행.
+		QHello qHello = QHello.hello;
+
+		// Querydsl을 사용하여 Hello 엔티티를 조회
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+	}
 }
