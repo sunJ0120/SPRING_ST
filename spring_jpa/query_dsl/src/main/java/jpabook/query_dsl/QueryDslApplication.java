5782b8c2ac54b2e8c87e77b5357bc56016677e5e
sunJ0120 <sspure1214@gmail.com>
2025-08-11 19:35:03 +0900

    Init : [실전! Query DSL] 초기 셋팅 완료

---
+++ spring_jpa/query_dsl/src/main/java/jpabook/query_dsl/QueryDslApplication.java
@@ -0,0 +1,13 @@
+package jpabook.query_dsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryDslApplication.class, args);
+	}
+
+}
---
+++ spring_jpa/query_dsl/src/main/java/jpabook/query_dsl/entity/Hello.java
@@ -0,0 +1,15 @@
+package jpabook.query_dsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Hello {
+    @Id
+    @GeneratedValue
+    private Long id;
+}
