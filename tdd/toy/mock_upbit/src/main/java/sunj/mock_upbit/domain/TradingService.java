52671cdcf0ed2a4f2ab748211bdaea0e409cbb2b
sunJ0120 <sspure1214@gmail.com>
2025-10-21 00:34:16 +0900

    feat: 주문 서비스에 TODO로 구현 기능 정의, 주입 객체 설정

--- tdd/toy/mock_upbit/src/main/java/sunj/mock_upbit/domain/TradingService.java
+++ tdd/toy/mock_upbit/src/main/java/sunj/mock_upbit/domain/TradingService.java
@@ -1,5 +1,9 @@
 package sunj.mock_upbit.domain;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sunj.mock_upbit.domain.dto.OrderResponse;
+
 /**
  * 클래스 이름: TradingService
  * <p>
@@ -7,5 +11,16 @@ package sunj.mock_upbit.domain;
  * <p>
  * 날짜: 2025-10-20
  */
+@Service
+@RequiredArgsConstructor
 public class TradingService {
+    private final BalanceValidator balanceValidator;    // Calculator는 Validator가 직접 쓰므로, 여기서 정의한다.
+    private final UpbitClient upbitClient;
+
+    public OrderResponse buy(String market, String price){
+        // TODO : upbitClient로 잔액 조회
+        // TODO : balanceValidator로 잔액과 주문 금액 예외 확인
+        // TODO : upbitClient로 주문
+        // TODO : 주문 결과 Response로 담아서 반환
+    }
 }
