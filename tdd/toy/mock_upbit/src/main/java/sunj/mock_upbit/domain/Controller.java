6cbb323954299f211dc4efc81462c43d2597f521
sunJ0120 <sspure1214@gmail.com>
2025-10-21 00:32:27 +0900

    feat: 요청단인 controller의 주문 기능 구현

--- tdd/toy/mock_upbit/src/main/java/sunj/mock_upbit/domain/Controller.java
+++ tdd/toy/mock_upbit/src/main/java/sunj/mock_upbit/domain/Controller.java
@@ -1,5 +1,14 @@
 package sunj.mock_upbit.domain;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sunj.mock_upbit.domain.dto.OrderRequest;
+import sunj.mock_upbit.domain.dto.OrderResponse;
+
 /**
  * 클래스 이름: Controller
  * <p>
@@ -7,5 +16,15 @@ package sunj.mock_upbit.domain;
  * <p>
  * 날짜: 2025-10-20
  */
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class Controller {
+    private final TradingService tradingService;
+    // TODO : 유저가 특정 상품 특정 갯수 거래 신청 요청을 한다.
+    @PostMapping("/order")
+    public ResponseEntity<OrderResponse> buyMarketOrder(@RequestBody OrderRequest orderRequest) {
+        OrderResponse response = tradingService.buy(orderRequest.getMarket(), orderRequest.getPrice());
+        return ResponseEntity.ok(response);
+    }
 }
